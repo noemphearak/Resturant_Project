c39e6c579210b6b6bd8ef1dcd140c0da26e6306a
noemphearak <noemphearakruppedu@gmail.com>
2018-05-06 17:32:19 +0700

    Update class Booking

--- BookingRestaurant.java
+++ BookingRestaurant.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingRestaurant {
-
+	private int id;
+	private List<TableRestaurant> reserveTables = new ArrayList<>();
+	private String customerName;
+	private String contactNumber;
+	
+	public BookingRestaurant(int id, String customerName, String contactNumber) {
+		this.id = id;
+		this.customerName = customerName;
+		this.contactNumber = contactNumber;
+	}
+	
+	public List<TableRestaurant> getReserveTable(List<TableRestaurant> reserveTables) {
+		return reserveTables;
+	}
+	
+	public TableRestaurant setReserveTable(TableRestaurant tb) {
+		return tb;
+	}
+	
+	public String toString() {
+		return "ID: "+ id +" CustomerName: "+ customerName +" ContactNumuber: "+ contactNumber;
+	}
 }
