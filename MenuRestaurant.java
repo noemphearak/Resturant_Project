aacd497861e15ccefd97f44483ab36b6510d4b07
noemphearak <noemphearakruppedu@gmail.com>
2018-05-15 22:36:11 +0700

    Updated Order

--- MenuRestaurant.java
+++ MenuRestaurant.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MenuRestaurant {
+	// TODO Create object from each class
+	private List<TableRestaurant> listTables 	= new ArrayList<TableRestaurant>();
+	private List<FoodRestaurant>  listFoods		= new ArrayList<FoodRestaurant>();
+	private ReservationRestaurant	reservation	= new ReservationRestaurant();
+	private List<ReservationRestaurant> listReservations	= new ArrayList<ReservationRestaurant>();
+	private List<OrderRestaurant> listOrders	= new ArrayList<OrderRestaurant>();
 	
+	// TODO constructor for init what run first in program
 	public MenuRestaurant() {
-		// Declaration variable as global
 		int inputKey = 0;
 		Scanner scanner	= new Scanner(System.in);
 		
@@ -13,20 +21,28 @@ public class MenuRestaurant {
 			System.out.println("You choose menu number: "+ inputKey);
 			switch(inputKey) {
 			case 1: 
+				createNewTable();
 				break;
 			case 2: 
+				listAllTable();
 				break;
 			case 3: 
+				createNewFood();
 				break;
 			case 4: 
+				listAllNewFood();
 				break;
 			case 5: 
+				takeReservation();
 				break;
 			case 6: 
+				listAllReservation();
 				break;
 			case 7: 
+				takeOrderFood();
 				break;
 			case 8: 
+				listAllOrder();
 				break;
 			case 9: 
 				endMenu();
@@ -41,9 +57,9 @@ public class MenuRestaurant {
 		}
 	}
 	
-	// Menu restaurant
+	// TODO Menu restaurant
 	public void mainMenu() {
-		System.out.println("=============================== Welcome to our Restaurant ===========================");
+		System.out.println("================================ Restaurant Reservation =============================");
         System.out.println("1). New Table ");
         System.out.println("2). List of Table");
         System.out.println("3). New Food");
@@ -55,13 +71,48 @@ public class MenuRestaurant {
         System.out.println("9). Exit program");
 	}
 	
+	// TODO method end menu 
 	public void endMenu() {
 		System.out.println("======================================= Thank you ===================================");
 	}
 	
+	// TODO error message
 	public void errMessage() {
 		System.out.println("=================================================");
 		System.out.println("| Error**: Invaild number! Please try it again. |");
 		System.out.println("=================================================");
+	}
+	
+	// TODO all method for each function restaurant
+	public void createNewTable() {
+		
+	}
+	
+	public void listAllTable() {
+		
+	}
+
+	public void createNewFood() {
+		
+	}
+	
+	public void listAllNewFood() {
+		
+	}
+	
+	public void takeReservation() {
+		
+	}
+	
+	public void listAllReservation() {
+		
+	}
+	
+	public void takeOrderFood() {
+		
+	}
+
+	public void listAllOrder() {
+		
 	}
 }
