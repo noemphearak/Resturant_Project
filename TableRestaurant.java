cd39a7bfe2878c1b12a543cd4254843e87741251
noemphearak <noemphearakruppedu@gmail.com>
2018-05-15 22:37:32 +0700

    Updated table

--- TableRestaurant.java
+++ TableRestaurant.java
@@ -1,4 +1,24 @@
 
 public class TableRestaurant {
-
+	private int numSeat;
+	private String id;
+	boolean available;
+	
+	public TableRestaurant(int numSeat, String id, boolean available) {
+		this.numSeat	= numSeat;
+		this.id			= id;
+		this.available	= available;
+	}
+	
+	public void setOccupled(boolean busy) {
+		this.available	= busy;
+	}
+	
+	public boolean getOccupled() {
+		return true;
+	}
+	
+	public String toString() {
+		return "Num Seat: "+ numSeat +" Id: "+ id +" Available: "+ available;
+	}
 }
