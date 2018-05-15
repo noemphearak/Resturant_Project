39931cf4cf9deec874da799b207e3ab4abfcb8d4
noemphearak <noemphearakruppedu@gmail.com>
2018-05-15 22:25:20 +0700

    Updated Food

--- FoodRestaurant.java
+++ FoodRestaurant.java
@@ -1,4 +1,16 @@
 
 public class FoodRestaurant {
-
+	private String id;
+	private String name;
+	private double price;
+	
+	public FoodRestaurant(String id, String name, double price) {
+		this.id		= id;
+		this.name 	= name;
+		this.price	= price;
+	}
+	
+	public String toString() {
+		return "ID: "+ id +" Food name: "+ name +" Price: "+ price;  
+	}
 }
