96d2a222c5b5b899e6172e44269d974c9c7023a4
noemphearak <noemphearakruppedu@gmail.com>
2018-05-16 22:57:12 +0700

    Updated interface

--- InterfaceRestaurant.java
+++ InterfaceRestaurant.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public interface InterfaceRestaurant {
-	public void create();
-	public void read();
-	public void update();
-	public void delete();
+	public void create(FoodRestaurant food);
+	public void read(List<FoodRestaurant> foodLists);
+	public void update(FoodRestaurant update);
+	public void delete(FoodRestaurant delete);
+	public void create(BookingRestaurant add);
+	public void read(ArrayList<BookingRestaurant> bookLists);
+	public void update(BookingRestaurant update);
+	public void delete(BookingRestaurant delete);
 }
