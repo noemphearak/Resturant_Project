1cd03438baa7c5704ae26b1cf6f8f65e42a030fa
noemphearak <noemphearakruppedu@gmail.com>
2018-05-16 22:42:03 +0700

    Updated Interface

--- InterfaceRestaurant.java
+++ InterfaceRestaurant.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
 
 public interface InterfaceRestaurant {
 	public void create();
 	public void read();
 	public void update();
 	public void delete();
-	public void create(FoodRestaurant food);
 }
