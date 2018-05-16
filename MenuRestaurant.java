df81116e64e5cac06fb6ff3c79c92b3f3cc949c1
noemphearak <noemphearakruppedu@gmail.com>
2018-05-16 20:19:43 +0700

    Updated Food

--- MenuRestaurant.java
+++ MenuRestaurant.java
@@ -89,11 +89,12 @@ public class MenuRestaurant {
 		int numSeat;
 		String id;
 		String enterd = "";
-		System.out.println("   ================================ Create New Table =============================");
+		System.out.println("==================================== Create New Table ===============================");
 		do {
 			System.out.print("Enter table No: "); id = scanner.nextLine();
 			System.out.print("Enter number of Seats: "); numSeat = scanner.nextInt(); scanner.nextLine();
 			listTables.add(new TableRestaurant(numSeat, id, false));
+			System.out.println("=====================================================================================");
 			System.out.print("Do you want to add more table? [Yes/No]: "); enterd = scanner.nextLine();
 		}while(!enterd.equals("no"));
 		if(enterd.equals("no")) {
@@ -104,25 +105,72 @@ public class MenuRestaurant {
 	public void listAllTable() {
 		String continues = "";
 		System.out.println("====================================== All Table ====================================");
-		do {
-			System.out.println("["+ "No"+ "\t\t"+ "Number of Seat"+ "\t\t"+ "Status"+"]");
-			for(TableRestaurant table : listTables) {
-				System.out.println(table.toString());
+		if(listTables.size() == 0) {
+			System.out.println("===============================================");
+			System.out.println("| Warning*: No table! Please create it first. |");
+			System.out.println("===============================================");
+		}else {
+			do {
+				System.out.println("["+ "No"+ "\t\t"+ "Number of Seat"+ "\t\t"+ "Status"+"]");
+				for(TableRestaurant table : listTables) {
+					System.out.println(table.toString());
+				}
+				System.out.println("=====================================================================================");
+				System.out.print("Do you want continue to Main Menu?[Yes/No]: "); continues = scanner.nextLine();
+				if(continues.equals("yes")) {
+					new MenuRestaurant();
+				}
+			}while(!continues.equals("no"));
+			if(continues.equals("no")) {
+				System.out.println("======================================== Exited =====================================");
+				System.exit(0);
 			}
-			System.out.print("Do you want continue to Main Menu?[Yes/No]: "); continues = scanner.nextLine();
-		}while(!continues.equals("no"));
-		if(continues.equals("no")) {
-			endMenu();
-			System.exit(0);
 		}
 	}
 
 	public void createNewFood() {
-		
+		String id		= "";
+		String name		= "";
+		double price 	= 0.0;
+		String enterd 	= "";
+		System.out.println("==================================== Create New Food ================================");
+		do {
+			System.out.print("Enter food Id: "); id = scanner.nextLine();
+			System.out.print("Enter food Name: "); name = scanner.nextLine();
+			System.out.print("Enter food Price: "); price = scanner.nextDouble();scanner.nextLine();
+			listFoods.add(new FoodRestaurant(id, name, price));
+			System.out.println("=====================================================================================");
+			System.out.print("Do you want to create more food? [Yes/No]: "); enterd = scanner.nextLine();
+		}while(!enterd.equals("no"));
+		if(enterd.equals("no")) {
+			System.out.println("Food created!!!");
+		}
 	}
 	
 	public void listAllNewFood() {
-		
+		String continues = "";
+		System.out.println("====================================== All Table ====================================");
+		if(listFoods.size() == 0) {
+			System.out.println("==============================================");
+			System.out.println("| Warning*: No food! Please create it first. |");
+			System.out.println("==============================================");
+		}else {
+			do {
+				System.out.println("["+ "Code"+ "\t\t"+ "Food Name"+ "\t\t"+ "Price"+"]");
+				for(FoodRestaurant food : listFoods) {
+					System.out.println(food.toString());
+				}
+				System.out.println("=====================================================================================");
+				System.out.print("Do you want continue to Main Menu?[Yes/No]: "); continues = scanner.nextLine();
+				if(continues.equals("yes")) {
+					new MenuRestaurant();
+				}
+			}while(!continues.equals("no"));
+			if(continues.equals("no")) {
+				System.out.println("======================================== Exited =====================================");
+				System.exit(0);
+			}
+		}
 	}
 	
 	public void takeReservation() {
