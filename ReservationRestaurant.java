bfedc507e670490ce897b6902f184b7fd26e9553
noemphearak <noemphearakruppedu@gmail.com>
2018-05-16 22:56:14 +0700

    Updated

--- ReservationRestaurant.java
+++ ReservationRestaurant.java
@@ -2,43 +2,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationRestaurant implements InterfaceRestaurant{
+	// TODO Create object list from BookingRestaurant 
 	private List<BookingRestaurant> reservationLists = new ArrayList<BookingRestaurant>();
 	
 	public ReservationRestaurant() {}
 	
+	@Override
 	public void create(BookingRestaurant add) {
 
 	}
 	
+	@Override
 	public void read(ArrayList<BookingRestaurant> bookLists) {
 		
 	}
 
+	@Override
 	public void update(BookingRestaurant update) {
 		
 	}
 
+	@Override
 	public void delete(BookingRestaurant delete) {
 		
 	}
 
 	@Override
-	public void create() {
+	public void create(FoodRestaurant food) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void read() {
+	public void read(List<FoodRestaurant> read) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void update() {
+	public void update(FoodRestaurant update) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void delete() {
+	public void delete(FoodRestaurant delete) {
+		// TODO Auto-generated method stub
 		
 	}
 }
